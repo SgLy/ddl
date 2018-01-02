6574defc54799150de10c06750f974189b73ae43
SgLy <775150558@qq.com>
2018-01-02 23:33:26 +0800

    register

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
 const mysql = require('mysql');
 const connection = mysql.createConnection({
@@ -10,11 +14,33 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-app.get('/api/user', (req, res) => {
-    res.send('GET user');
-    console.log(req.query);
+function GG(res, err) {
+    res.json({
+        status: -1,
+        reason: 'Error!'
+    });
+}
+
+/* USER */
+app.put('/api/user', (req, res) => {
+    console.log('POST /api/user', req.body);
+    const q = 'INSERT INTO user ( username, password, nickname ) VALUE (?, ?, ?)';
+    const data = [req.body.username, req.body.password, req.body.nickname];
+    connection.query(q, data, (err, rows) => {
+        if (err) GG(res, err);
+        const q = 'SELECT * FROM user WHERE username = ?';
+        connection.query(q, [req.body.username], (err, rows) => {
+            if (err) GG(res, err);
+            res.json({
+                status: 1,
+                reason: 'Success',
+                id: rows[0].id
+            });
+        });
+    });
 });
 
+/* GLOBAL */
 app.get('/', (req, res) => {
     res.send('DDL backend working.');
 });
