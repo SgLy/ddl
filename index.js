2b7c315581b83b37ec7e341d9b82f48b31d3a586
SgLy <775150558@qq.com>
2018-01-03 01:40:41 +0800

    finish notices

--- index.js
+++ index.js
@@ -85,6 +85,55 @@ app.post('/api/deadline', (req, res) => {
     });
 });
 
+/* NOTICE */
+app.get('/api/notice', (req, res) => {
+    console.log('GET /api/notice', req.body);
+
+    const q = `
+        SELECT * FROM notice
+        LEFT JOIN user_course ON notice.course_id = user_course.course_id
+        LEFT JOIN user ON notice.user_id = user.id OR user_course.user_id = user.id
+        WHERE user.token = ?
+    `;
+    const data = [req.body.token];
+    connection.query(q, data, (err, result) => {
+        if (err) {
+            GG(res, err);
+            return;
+        }
+        res.json({
+            status: 1,
+            reason: 'Success',
+            notices: result.map(r => ({
+                id: r.id,
+                title: r.title,
+                description: r.description
+            }))
+        });
+    });
+});
+app.post('/api/notice', (req, res) => {
+    console.log('POST /api/notice', req.body);
+    const q = `
+        INSERT INTO notice (title, description, course_id, user_id)
+        VALUE (?, ?, NULL, (SELECT id FROM user WHERE token = ?));
+    `;
+    const data = [req.body.title, req.body.description, req.body.token];
+    connection.query(q, data, (err, result) => {
+        if (err) {
+            GG(res, err);
+            return;
+        }
+        if (result.affectedRows === 1) {
+            res.json({
+                status: 1,
+                reason: 'Success',
+                id: result.insertId
+            });
+        } else GG(res, 'Token error');
+    });
+});
+
 /* USER */
 app.put('/api/user', (req, res) => {
     console.log('POST /api/user', req.body);
@@ -143,7 +192,7 @@ app.get('/api/user', (req, res) => {
 app.get('/api/login', (req, res) => {
     console.log('GET /api/login', req.body);
     const token = uuid();
-    console.log(token);
+    console.log(`Generate UUID: ${token}`);
     const q = 'UPDATE user SET token = ? WHERE username = ? AND password = ?';
     const data = [token, req.body.username, req.body.password];
     connection.query(q, data, (err, result) => {
